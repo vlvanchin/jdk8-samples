e5a1d4938a58ea52051ecb3d6feef821569b035e
vlvanchin <vlvanchin+work@gmail.com>
2022-01-27 19:06:23 +0800

    updated code
    Signed-off-by: vlvanchin <vlvanchin+work@gmail.com>

--- src/com/vlv/generic/GenericMethods.java
+++ src/com/vlv/generic/GenericMethods.java
@@ -28,18 +28,25 @@ public class GenericMethods <T> {
 	// Some implementation over strings heres
 	}
 	
-	public <T> void action(final T action) {
+	// this fails as the signature is same as above
+	void sort ( Collection < Number> numbers ) {
+		// do some thing here
+	}
+	
+	// the following method fails the compilation when used with instanceof 
+	public <T> void action_01(final T action) {
 		if (action instanceof T) {
 			// Do something here
 		}
 	}
 
-	public< T > void action( final T action ) {
+	public< T > void action_02( final T action ) {
 		if( T.class.isAssignableFrom( Number.class ) {
 		// Do something here
 		}
 	}
 	
+	// cannot create a generic arrya of T
 	public< T > void performAction_01( final T action ) {
 		T[] actions = new T[ 0 ];
 	}
